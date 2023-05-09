a954d448638a0946f043c3b7c4eb729ab8dfdcd0
TKMshengu <tokocha81@gmail.com>
2023-05-09 23:15:11 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,10 +3,10 @@
 import requests
 
 def number_of_subscribers(subreddit):
-    """Return the toal number on a given Reddit subreddit."""
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-            "User-Agent": "Linux:0x16.api.advance:v10.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
