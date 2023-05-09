6d6366bd13b9c94601313168df6bf30799f07326
“Tokologo1” <tokocha81@gmail.com>
2023-05-09 14:11:03 -0700

    status

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Function to print hot posts on a given Reddit subreddit"""
-import request
+import requests
 
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
@@ -12,7 +12,7 @@ def top_ten(subreddit):
     param = {
         "limit": 10
     }
-    response = request.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
 
     if response.status_code == 404
         print("None")
